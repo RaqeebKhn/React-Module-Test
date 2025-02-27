6a355aea71fb47c13dd111d41209ff2cb67631f2
RaqeebKhn <joysouls.rk@gmail.com>
2025-02-28 00:22:30 +0530

    empty sidebar list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { useRef } from 'react'
 import MainPage from './components/MainPage/MainPage'
 import CreateNoteButton from './components/CreateNoteButton/CreateNoteButton'
 import Sidebar from './components/SideBar/SideBar'
 import './App.css'
 
 function App() {
+  const sidebarRef = useRef()
   const handleCreateGroup = (newGroup) => { 
-    
+    sidebarRef.current.handleCreateGroup(newGroup) 
   } 
   return (
     <div className="app-container">
-      <Sidebar />
+       <Sidebar ref={sidebarRef} />
       <main className="main-content">
         <MainPage />
         <CreateNoteButton onCreateGroup={handleCreateGroup} />
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,29 +1,19 @@
-import { useState } from 'react'
+import { useState, forwardRef, useImperativeHandle } from 'react'
 import NoteGroup from '../NoteGroup/NoteGroup'
 import './Sidebar.css'
 
-const Sidebar = () => {
+const Sidebar = forwardRef((props, ref) => { 
   const [selectedGroup, setSelectedGroup] = useState(null)
-  const [noteGroups, setNoteGroups] = useState([
-    { id: 1, title: 'My Notes', color: '#0047FF' },
-    { id: 2, title: 'My personal grp', color: '#B38BFA' },
-    { id: 3, title: 'Javascript grp', color: '#FF79F2' },
-    { id: 4, title: 'HTML grp', color: '#43E6FC' },
-    { id: 5, title: 'CSS Notes', color: '#F19576' },
-    { id: 6, title: 'SQL Notes', color: '#6691FF' },
-    { id: 7, title: 'Python Notes', color: '#FF4ECD' },
-    { id: 8, title: 'Python Notes', color: '#FF4ECD' },
-    { id: 9, title: 'Python Notes', color: '#FF4ECD' },
-    { id: 10, title: 'Python Notes', color: '#FF4ECD' },
-    { id: 11, title: 'Python Notes', color: '#FF4ECD' },
-    { id: 12, title: 'Python Notes', color: '#FF4ECD' },
-    { id: 13, title: 'Python Notes', color: '#FF4ECD' }
-  ])
+  const [noteGroups, setNoteGroups] = useState([])
 
-  const handleCreateGroup = (newGroup) => { 
-    const newId = noteGroups.length + 1 
-    setNoteGroups([...noteGroups, { ...newGroup, id: newId }]) 
-  } 
+  const handleCreateGroup = (newGroup) => {
+    const newId = noteGroups.length + 1
+    setNoteGroups([...noteGroups, { ...newGroup, id: newId }])
+  }
+
+  useImperativeHandle(ref, () => ({ 
+    handleCreateGroup 
+  }))
 
   return (
     <div className="sidebar">
@@ -43,6 +33,8 @@ const Sidebar = () => {
       </div>
     </div>
   )
-}
+})
+
+Sidebar.displayName = 'Sidebar'
 
 export default Sidebar
